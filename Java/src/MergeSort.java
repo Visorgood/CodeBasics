435888a82073a8518e4201fdd51de380c5f11098
Viacheslav <slavanw@gmail.com>
2014-01-31 13:58:19 +0100

    MergeSort update

--- Java/src/MergeSort.java
+++ Java/src/MergeSort.java
@@ -8,28 +8,25 @@ public class MergeSort
 
 	private static void Merge(int[] array, int s, int e)
 	{
-		if (e - s <= 1)
+		if (e - s < 1)
 			return;
 		
-		int middle = (e + s) / 2;
-		Merge(array, s, middle);
-		Merge(array, middle + 1, e);
+		int m = (e + s) / 2;
+		Merge(array, s, m);
+		Merge(array, m + 1, e);
 		
 		int[] merged = new int[e - s + 1];
-		int i = s, j = middle + 1;
+		int i = s, j = m + 1;
 		for (int k = 0; k < merged.length; ++k)
 		{
-			if (i > middle)
+			if (i > m)
 				merged[k] = array[j++];
 			else if (j > e)
 				merged[k] = array[i++];
 			else
-			{
-				if (array[i] < array[j])
-					merged[k] = array[i++];
-				else
-					merged[k] = array[j++];
-			}	
+				merged[k] = (array[i] < array[j] ? array[i++] : array[j++]);
 		}
+		for (int k = 0; k < merged.length; ++k)
+			array[s + k] = merged[k];
 	}
 }
