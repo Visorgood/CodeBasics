cc77e22fa30a4ce93565fffd734a1d79247f4939
Viacheslav <slavanw@gmail.com>
2014-10-11 17:23:21 +0200

    BinarySearch.py updated

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -5,7 +5,7 @@
 
 def BinarySearch(A, x):
       return BinarySearchRec(A, 0, len(A) - 1, x)
-      
+
 def BinarySearchRec(A, start, end, x):
       if start > end:
             return -1
@@ -16,3 +16,14 @@ def BinarySearchRec(A, start, end, x):
             return BinarySearchRec(A, start, i - 1, x)
       else:
             return BinarySearchRec(A, i + 1, end, x)
+
+import random
+A = [0]
+N = 50
+for i in range(1, N):
+    A.append(A[i - 1] + random.randint(1, 3))
+print "Array:", A
+
+x = random.randrange(A[N - 1] + 1)
+print "x =", x
+print "index of x is", BinarySearch(A, x)
