aec21f068a034e853e07cc002c2fa32a15a6b37c
Viacheslav <slavanw@gmail.com>
2014-10-11 21:54:45 +0200

    binarysearch and mergesort in Python updated

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -1,29 +1,31 @@
-# A - array of values
+# array - array of values
 # x - value to find
 # return value is an index of matched element
 # if x is not in A, then -1 is returned
 
-def BinarySearch(A, x):
-      return BinarySearchRec(A, 0, len(A) - 1, x)
 
-def BinarySearchRec(A, start, end, x):
-      if start > end:
-            return -1
-      i = (start + end) / 2
-      if A[i] == x:
-            return i
-      elif A[i] > x:
-            return BinarySearchRec(A, start, i - 1, x)
-      else:
-            return BinarySearchRec(A, i + 1, end, x)
+def binarysearch(array, x):
+    return binarysearchrec(array, 0, len(array) - 1, x)
+
+
+def binarysearchrec(array, start, end, x):
+    if start > end:
+        return -1
+    i = (start + end) / 2
+    if array[i] == x:
+        return i
+    elif array[i] > x:
+        return binarysearchrec(array, start, i - 1, x)
+    else:
+        return binarysearchrec(array, i + 1, end, x)
 
 import random
-A = [0]
-N = 50
-for i in range(1, N):
-    A.append(A[i - 1] + random.randint(1, 3))
-print "Array:", A
+array = [0]
+length = 50
+for i in range(1, length):
+    array.append(array[i - 1] + random.randint(1, 3))
+print "Array:", array
 
-x = random.randrange(A[N - 1] + 1)
+x = random.randrange(array[length - 1] + 1)
 print "x =", x
-print "index of x is", BinarySearch(A, x)
+print "index of x is", binarysearch(array, x)
--- Python/MergeSort.py
+++ Python/MergeSort.py
@@ -1,32 +1,40 @@
-def Merge(B, C):
-	D = []
-	lenB = len(B)
-	lenC = len(C)
-	i = 0
-	j = 0
-	while i < lenB and j < lenC:
-		if B[i] < C[j]:
-			D.append(B[i])
-			i += 1
-		else:
-			D.append(C[j])
-			j += 1
-	if i < lenB:
-		while i < lenB:
-			D.append(B[i])
-			i += 1
-	else:
-		while j < lenC:
-			D.append(C[j])
-			j += 1
-	return D
+def mergesort(array):
+    if len(array) == 1:
+        return array
+    else:
+        middle = len(array) / 2
+        half1 = mergesort(array[:middle])
+        half2 = mergesort(array[middle:])
+        mergedarray = merge(half1, half2)
+        return mergedarray
 
-def MergeSort(A):
-	if len(A) == 1:
-		return A
-	else:
-		half = len(A) / 2
-		B = MergeSort(A[:half])
-		C = MergeSort(A[half:])
-		D = Merge(B, C)
-		return D
+
+def merge(half1, half2):
+    mergedarray = []
+    lenhalf1 = len(half1)
+    lenhalf2 = len(half2)
+    i = 0
+    j = 0
+    while i < lenhalf1 and j < lenhalf2:
+        if half1[i] < half2[j]:
+            mergedarray.append(half1[i])
+            i += 1
+        else:
+            mergedarray.append(half2[j])
+            j += 1
+    if i < lenhalf1:
+        while i < lenhalf1:
+            mergedarray.append(half1[i])
+            i += 1
+    else:
+        while j < lenhalf2:
+            mergedarray.append(half2[j])
+            j += 1
+    return mergedarray
+
+import random
+array = []
+for i in range(50):
+    array.append(random.randrange(100))
+print "Array:", array
+print "Sorted array:", mergesort(array)
