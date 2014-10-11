8115ce91aa2768daae3293977fea2dcdae5a8b28
Viacheslav <slavanw@gmail.com>
2014-10-11 19:08:11 +0200

    CPlusPlus project added

---
+++ CPlusPlus/src/CPlusPlus.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : CPlusPlus.cpp
+// Author      : Viacheslav Inozemtsev
+// Version     :
+// Copyright   : This project is the property of Viacheslav Inozemtsev
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
