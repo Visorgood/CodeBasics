773472f0464cd8f4da2802f1c265d0ad78a0ae0b
Viacheslav <slavanw@gmail.com>
2014-10-27 15:18:30 +0100

    Subsets.java updated

--- Java/src/Subsets.java
+++ Java/src/Subsets.java
@@ -1,21 +1,46 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Iterator;
 
-public class Subsets
-{
-	public static Iterator<String> generate(String str)	{
-		ArrayList<String> subsets = new ArrayList<String>();
-		
-		if (str == null || str.length() == 0)
-			return subsets.iterator();
-		
-		subsets.add("");
-		for (char c : str.toCharArray()) {
-			int n = subsets.size();
-			for (int i = 0; i < n; ++i)
-				subsets.add(subsets.get(i) + c);
-		}
-		
-		return subsets.iterator();
-	}
+public class Subsets {
+  public static Iterator<String> generate(String str) {
+    if (str == null) {
+      return null;
+    }
+    LinkedList<String> subsets = new LinkedList<String>();
+    StringBuilder sb = new StringBuilder();
+    generate(str, 0, sb, subsets);
+    return subsets.iterator();
+  }
+  
+  public static void generate(String str, int i, StringBuilder sb, LinkedList<String> subsets) {
+    subsets.addLast(sb.toString());
+    if (i == str.length()) {
+      return;
+    }
+    for (int j = i; j < str.length(); ++j) {
+      sb.append(str.charAt(j));
+      generate(str, j + 1, sb, subsets);
+      sb.setLength(sb.length() - 1);
+    }
+  }
+  
+  public static void print(String str) {
+    if (str == null) {
+      return;
+    }
+    StringBuilder sb = new StringBuilder();
+    print(str, 0, sb);
+  }
+
+  private static void print(String str, int i, StringBuilder sb) {
+    System.out.println(sb.toString());
+    if (i == str.length()) {
+      return;
+    }
+    for (int j = i; j < str.length(); ++j) {
+      sb.append(str.charAt(j));
+      print(str, j + 1, sb);
+      sb.setLength(sb.length() - 1);
+    }
+  }
 }
