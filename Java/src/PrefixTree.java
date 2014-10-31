fdac06ce8923829f14ff9f83bc3c33ee3d115693
Viacheslav <slavanw@gmail.com>
2014-10-31 12:49:34 +0100

    PrefixTree.java updated

--- Java/src/PrefixTree.java
+++ Java/src/PrefixTree.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 
+// only non-negative values are allowed
+// -1 is reserved as a flag that node is not a key node
 public class PrefixTree {
   private final Node root;
 
@@ -7,7 +9,7 @@ public class PrefixTree {
     root = new Node("", null);
   }
 
-  public void insert(String key) {
+  public void insert(String key, int value) {
     if (key == null || key.length() == 0) {
       return;
     }
@@ -19,7 +21,7 @@ public class PrefixTree {
       }
       node = node.children.get(symbol);
     }
-    node.isKey = true;
+    node.value = value;
   }
 
   public void delete(String key) {
@@ -34,38 +36,38 @@ public class PrefixTree {
         return;
       }
     }
-    node.isKey = false;
-    while (node.children.size() == 0 && !node.isKey) {
+    node.value = -1;
+    while (node.children.size() == 0 && node.value == -1) {
       String symbol = node.symbol;
       node = node.parent;
       node.children.remove(symbol);
     }
   }
 
-  public boolean exists(String key) {
+  public int get(String key) {
     if (key == null || key.length() == 0) {
-      return false;
+      return -1;
     }
     Node node = root;
     for (int i = 0; i < key.length(); ++i) {
       String symbol = key.substring(i, i + 1);
       node = node.children.get(symbol);
       if (node == null) {
-        return false;
+        return -1;
       }
     }
-    return node.isKey;
+    return node.value;
   }
 
   private class Node {
     String symbol;
-    boolean isKey;
+    int value;
     Node parent;
     HashMap<String, Node> children;
 
     Node(String symbol, Node parent) {
       this.symbol = symbol;
-      this.isKey = false;
+      this.value = -1;
       this.parent = parent;
       this.children = new HashMap<String, Node>();
     }
