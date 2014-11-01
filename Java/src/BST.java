fa612b8ae889cec4dd91bb9b0cb813a1002cad1b
Viacheslav <slavanw@gmail.com>
2014-11-01 22:47:48 +0100

    BST.java updated

--- Java/src/BST.java
+++ Java/src/BST.java
@@ -55,31 +55,33 @@ class BST<T extends Comparable<T>> {
   }
 
   public T min() {
-    return min(this.root);
+    Node minNode = findMinNode(this.root);
+    return (minNode != null ? minNode.key : null);
   }
 
-  private T min(Node node) {
+  private Node findMinNode(Node node) {
     if (null == node) {
       return null;
     }
     while (null != node.left) {
       node = node.left;
     }
-    return node.key;
+    return node;
   }
 
   public T max() {
-    return max(this.root);
+    Node maxNode = findMaxNode(this.root);
+    return (maxNode != null ? maxNode.key : null);
   }
 
-  private T max(Node node) {
+  private Node findMaxNode(Node node) {
     if (null == node) {
       return null;
     }
     while (null != node.right) {
       node = node.right;
     }
-    return node.key;
+    return node;
   }
 
   public T predecessor(T key) {
@@ -88,7 +90,7 @@ class BST<T extends Comparable<T>> {
       return null;
     }
     if (null != node.left) {
-      return max(node.left);
+      return findMaxNode(node.left).key;
     }
     while (null != node && !(node.key.compareTo(key) < 0)) {
       node = node.parent;
@@ -102,7 +104,7 @@ class BST<T extends Comparable<T>> {
       return null;
     }
     if (null != node.right) {
-      return min(node.right);
+      return findMinNode(node.right).key;
     }
     while (null != node && !(node.key.compareTo(key) > 0)) {
       node = node.parent;
@@ -126,23 +128,101 @@ class BST<T extends Comparable<T>> {
     return i;
   }
 
-  /*
-   * public void delete(T key) { Node node = null; while (null != (node = SearchNode(value))) { if
-   * (null == node.left && null == node.right) { if (node == this.root) this.root = null; else {
-   * Node parent = node.parent; if (node == parent.left) parent.left = null; else parent.right =
-   * null; while (null != parent) { --parent.size; parent = parent.parent; } } } else if ((null ==
-   * node.left) ^ (null == node.right)) { Node child = (null == node.left ? node.right : node.left);
-   * if (node == this.root) { this.root = child; this.root.parent = null; } else { Node parent =
-   * node.parent; if (node == parent.left) parent.left = child; else parent.right = child;
-   * child.parent = parent; while (null != parent) { --parent.size; parent = parent.parent; } } }
-   * else { throw new NotImplementedException(); Node pred = node.left; while (null != pred.right)
-   * pred = pred.right; Node predParent = pred.parent; if (pred != node.left) { if (null !=
-   * pred.left) pred.left.parent = pred.parent; pred.parent.right = pred.left; } Node parent =
-   * node.parent; if (node == parent.left) parent.left = pred; else parent.right = pred; pred.parent
-   * = parent; if (pred != node.left) pred.left = node.left; else predParent = pred; pred.right =
-   * node.right; while (null != predParent) { --predParent.size; predParent = predParent.parent; } }
-   * } }
-   */
+  public void delete(T key) {
+    Node node = null;
+    while (null != (node = searchNode(key))) {
+      if (null == node.left && null == node.right) {
+        deleteWithNoChildNodes(node);
+      } else if ((null == node.left) ^ (null == node.right)) {
+        deleteWithOneChildNode(node);
+      } else {
+        deleteWithTwoChildNodes(node);
+      }
+    }
+  }
+
+  private void deleteWithNoChildNodes(Node node) {
+    if (node == this.root) {
+      this.root = null;
+    } else {
+      Node parent = node.parent;
+      if (node == parent.left) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
+      node.parent = null;
+      while (null != parent) {
+        --parent.size;
+        parent = parent.parent;
+      }
+    }
+  }
+
+  private void deleteWithOneChildNode(Node node) {
+    Node child = (null != node.left ? node.left : node.right);
+    if (node == this.root) {
+      this.root = child;
+      this.root.parent = null;
+    } else {
+      Node parent = node.parent;
+      if (node == parent.left) {
+        parent.left = child;
+      } else {
+        parent.right = child;
+      }
+      node.parent = null;
+      child.parent = parent;
+      while (null != parent) {
+        --parent.size;
+        parent = parent.parent;
+      }
+    }
+  }
+
+  private void deleteWithTwoChildNodes(Node node) {
+    Node pred = node.left;
+    while (null != pred.right) {
+      pred = pred.right;
+    }
+    swap(node, pred);
+    if (null == node.left && null == node.right) {
+      deleteWithNoChildNodes(node);
+    } else {
+      deleteWithOneChildNode(node);
+    }
+  }
+
+  private void swap(Node node1, Node node2) {
+    Node node1Parent = node1.parent;
+    if (null != node1Parent) {
+      if (node1 == node1Parent.left) {
+        node1Parent.left = node2;
+      } else {
+        node1Parent.right = node2;
+      }
+    }
+    Node node2Parent = node2.parent;
+    if (null != node2Parent) {
+      if (node2 == node2Parent.left) {
+        node2Parent.left = node1;
+      } else {
+        node2Parent.right = node1;
+      }
+    }
+    node2.parent = node1Parent;
+    node1.parent = node2Parent;
+    Node t = node1.left;
+    node1.left = node2.left;
+    node2.left = t;
+    node1.left.parent = node2;
+    node2.left.parent = node1;
+    t = node1.right;
+    node1.right = node2.right;
+    node2.right = t;
+    node1.right.parent = node2;
+    node2.right.parent = node1;
+  }
 
   public T select(int i) {
     if (i <= 0 || i > size()) {
