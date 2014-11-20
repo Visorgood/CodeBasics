11e6655c776e6e0c12d1d03916598c0a0e958c56
Viacheslav <slavanw@gmail.com>
2014-11-20 16:20:02 +0100

    Update Convert.java

--- Java/src/Convert.java
+++ Java/src/Convert.java
@@ -1,5 +1,9 @@
 public class Convert {
-  public static int strToInt(String s) throws IllegalArgumentException  {
+  public static int strToInt(String s) throws IllegalArgumentException {
+    return strToInt(s, 10);
+  }
+
+  public static int strToInt(String s, int base) throws IllegalArgumentException {
     if (s == null || s.length() == 0) {
       throw new IllegalArgumentException("Input string is null or has the length 0!");
     }
@@ -13,18 +17,22 @@ public class Convert {
     long val = 0;
     for (int i = (minus ? 1 : 0); i < s.length(); ++i) {
       char c = s.charAt(i);
-      if (!Character.isDigit(c)) {
+      if (!isProperDigit(c, base)) {
         throw new IllegalArgumentException("Input string has invalid characters!");
       }
-      val = val * 10 + (s.charAt(i) - '0');
+      val = val * base + (Character.isDigit(c) ? s.charAt(i) - '0' : s.charAt(i) - 'A' + 10);
       if (val > Integer.MAX_VALUE) {
         throw new IllegalArgumentException("Integer overflow!");
       }
     }
-    return (int)(minus ? -val : val);
+    return (int) (minus ? -val : val);
   }
 
   public static String intToStr(int val) {
+    return intToStr(val, 10);
+  }
+
+  public static String intToStr(int val, int base) {
     if (val == 0) {
       return "0";
     }
@@ -35,12 +43,23 @@ public class Convert {
       minus = true;
     }
     while (val > 0) {
-      sb.append(val % 10);
-      val /= 10;
+      int digit = val % base;
+      sb.append((char)(digit < 10 ? '0' + digit : 'A' + digit - 10));
+      val /= base;
     }
     if (minus) {
       sb.append('-');
     }
     return sb.reverse().toString();
+  }
+
+  public static String toBase(String s, int b1, int b2) {
+    int val = strToInt(s, b1);
+    return intToStr(val, b2);
+  }
+
+  private static boolean isProperDigit(char c, int base) {
+    return (base <= 10 ? Character.isDigit(c) && (c - '0' < base) : Character.isDigit(c)
+        || (c - 'A' + 10 < base));
   }
 }
