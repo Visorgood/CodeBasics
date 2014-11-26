b3fb1a69c155a9f0d8d99b3afddd087d3e2f0bc8
Viacheslav <slavanw@gmail.com>
2014-11-26 11:42:07 +0100

    Update Main.java

--- GoogleCodeJam/src/Main.java
+++ GoogleCodeJam/src/Main.java
@@ -1,11 +1,11 @@
 import java.io.IOException;
 
-public class Main
-{
-	public static void main(String[] args) throws IOException
-	{
-		//new StoreCredit().solve();
-		//new ReverseWords().solve();
-		new MinimumScalarProduct().solve();
-	}
+public class Main {
+  public static void main(String[] args) throws IOException {
+    // new StoreCredit().solve();
+    // new ReverseWords().solve();
+    // new MinimumScalarProduct().solve();
+    // new AlienLanguage().solve();
+    new RopeIntranet().solve();
+  }
 }
