6a499aaebd020b5433dc185f7d27b00284d29275
Viacheslav <viacheslav.inozemtsev@gmail.com>
2014-12-08 19:04:30 +0100

    Update Main.java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,64 +1,9 @@
-import java.io.IOException;
-import java.util.BitSet;
-import java.util.Iterator;
 import java.util.Random;
 
-public class Main
-{
-	private static Boolean allCharsUniq(String str)
-	{
-		BitSet bs = new BitSet(256);
-		for (char c : str.toCharArray())
-		{
-			if (bs.get(c))
-				return false;
-			bs.set(c);
-		}
-		return true;
-	}
-
-	
-	public static void main(String[] args) throws IOException
-	{
-		//useSort(new MergeSort());
-		//useSort(new QuickSort());
-		//System.out.println(allCharsUniq("abcdefgh"));
-		//Iterator<String> it = Subsets.generate("abcde");
-		//Iterator<String> it = Permutations.generate("abcde");
-		//Iterator<String> it = PhoneNumberStringPermutations.generate("0123");
-		//while (it.hasNext())
-			//System.out.println(it.next());
-		int[][] matrix = new int[][] {
-				{0, 0, 0, 0},
-				{0, 0, 0, 0},
-				{0, 0, 0, 0},
-				{0, 0, 0, 0}};
-		long ways = CountWays.count(matrix);
-		System.out.println(ways);
-	}
-	
-	static void useSort(Sort sort)
-	{
-		Random r = new Random();
-		int n = 53;
-		int m = 60;
-		int[] array = new int[n];
-		System.out.print("Original array: ");
-		for (int i = 0; i < n; ++i)
-		{
-			int x = r.nextInt(m);
-			array[i] = x;
-			System.out.print(x);
-			System.out.print(" ");
-		}
-		System.out.println();
-		sort.execute(array);
-		System.out.print("Sorted array:   ");
-		for (int i = 0; i < n; ++i)
-		{
-			System.out.print(array[i]);
-			System.out.print(" ");
-		}
-		System.out.println();
-	}
+public class Main {
+  static Random r = new Random();
+  
+  public static void main(String[] args) {
+    
+  }
 }
